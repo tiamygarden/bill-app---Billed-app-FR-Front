8f95ec1e33e73a533e0aba148f7cd3565ef52f5f
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-10-28 19:53:30 +0200

    fixed issues

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -11,54 +11,52 @@ export default class {
         if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
         const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
         if (iconEye) iconEye.forEach(icon => {
-            icon.addEventListener('click', (e) => this.handleClickIconEye(icon))
+            icon.addEventListener('click', () => this.handleClickIconEye(icon))
         })
         new Logout({document, localStorage, onNavigate})
     }
 
-    handleClickNewBill = (e) => {
+    handleClickNewBill = () => {
         this.onNavigate(ROUTES_PATH['NewBill'])
     }
 
     handleClickIconEye = (icon) => {
         const billUrl = icon.getAttribute("data-bill-url")
-        const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
+        const imgWidth = Math.floor($('#modaleFile').width() * 0.4)
         $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
         $('#modaleFile').modal('show')
     }
 
     getBills = () => {
-        const userEmail = localStorage.getItem('user') ?
-            JSON.parse(localStorage.getItem('user')).email : ""
-
         if (this.store) {
             return this.store
                 .bills()
                 .list()
-                .then((snapshot) => {
-                    const bills = snapshot.map((doc) => {
-                        try {
-                            return {
-                                ...doc,
-                                date: formatDate(doc.date),
-                                status: formatStatus(doc.status)
+                .then(snapshot => {
+
+                    //sort by date DESC affichage ecran
+                    const bills = snapshot.sort((a, b) => (new Date(a.date) - new Date(b.date)))
+                        .map(doc => {
+                            try {
+                                return {
+                                    ...doc,
+                                    date: formatDate(doc.date),
+                                    status: formatStatus(doc.status)
+                                }
+                            } catch (e) {
+                                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                                // log the error and return unformatted date in that case
+                                console.log(e, 'for', doc)
+                                return {
+                                    ...doc,
+                                    date: doc.date,
+                                    status: formatStatus(doc.status)
+                                }
                             }
-                        } catch (e) {
-                            // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-                            // log the error and return unformatted date in that case
-                            console.log(e, 'for', doc)
-                            return {
-                                ...doc,
-                                date: doc.date,
-                                status: formatStatus(doc.status)
-                            }
-                        }
-                    })
-                        .filter((bill) => bill.email === userEmail)
+                        })
                     console.log('length', bills.length)
                     return bills
                 })
-                .catch((error) => error)
         }
     }
 }
